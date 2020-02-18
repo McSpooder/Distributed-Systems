e1602c9e58d14429adbe98b860eed9880b1c324d
danny-laptop <hextra@live.com>
2020-02-18 13:51:38 +0000

    Lab-0 commit

---
+++ Lab-0/Client/ClientProgram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Client
+{
+    class ClientProgram
+    {
+
+        static byte[] Serialize(String input)
+        {
+            byte[] mybyte = new byte[input.Length];
+            mybyte = System.Text.Encoding.ASCII.GetBytes(input);
+            return mybyte;
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Client Program.");
+
+            TcpClient client = new TcpClient();
+            client.Connect("localhost", 5002);
+
+            NetworkStream ns = client.GetStream();
+            byte[] mys = Serialize("Just a simple message passing through, woo!");
+            ns.Write(mys, 0, mys.Length);
+            ns.Flush();
+
+        }
+    }
+}
---
+++ Lab-0/Server/ServerProgram.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class ServerProgram
+    {
+
+        static string DecodeMessage(ref NetworkStream pNetStream)
+        {
+
+            String in_string = "";
+
+            byte[] in_buffer = new byte[1024]; //arbitrary buffer size
+
+            do
+            {
+                pNetStream.Read(in_buffer, 0, in_buffer.Length);
+                in_string += System.Text.UTF8Encoding.ASCII.GetString(in_buffer);
+
+            } while (pNetStream.DataAvailable);
+
+            return in_string;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Server Program.");
+            TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 5002);
+            listener.Start();
+
+            TcpClient in_client = listener.AcceptTcpClient();
+            NetworkStream in_stream = in_client.GetStream();
+
+            String in_str = DecodeMessage(ref in_stream);
+            Console.WriteLine(in_str);
+            Console.ReadLine();
+        }
+    }
+}
