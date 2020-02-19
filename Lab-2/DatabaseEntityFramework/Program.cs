fe8eb0166f6a9425616f9626cca3c26455b21293
danny-laptop <hextra@live.com>
2020-02-19 13:56:28 +0000

    Lab2 commit.

---
+++ Lab-2/DatabaseEntityFramework/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseEntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
