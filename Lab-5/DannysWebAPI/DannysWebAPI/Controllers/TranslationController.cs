9df2f2aa3a89f647ee189374173871d78ef799ed
danny-desktop <hextra@live.com>
2020-03-01 22:14:32 +0000

    Added a custom translation controller.

--- Lab-5/DannysWebAPI/DannysWebAPI/Controllers/TranslationController.cs
+++ Lab-5/DannysWebAPI/DannysWebAPI/Controllers/TranslationController.cs
@@ -2,41 +2,43 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DannysWebAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class ValuesController : ControllerBase
+    public class TranslationController : ControllerBase
     {
-        // GET api/values
+        // GET: api/Translation
         [HttpGet]
-        public ActionResult<IEnumerable<string>> Get()
+        public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        // GET: api/Translation/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
         {
-            return "value";
+            string result = string.Format("Your value plus 100 is: {0}", (100 + id).ToString());
+            return result;
         }
 
-        // POST api/values
+        // POST: api/Translation
         [HttpPost]
         public void Post([FromBody] string value)
         {
         }
 
-        // PUT api/values/5
+        // PUT: api/Translation/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody] string value)
         {
         }
 
-        // DELETE api/values/5
+        // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
