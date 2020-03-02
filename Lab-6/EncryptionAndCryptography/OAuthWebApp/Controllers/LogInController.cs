ba027931a8bfafe5afe3dd0f9cf9836208a6a927
danny-desktop <hextra@live.com>
2020-03-02 03:28:35 +0000

    Initial Lab-6 commit. It doesnt work...

---
+++ Lab-6/EncryptionAndCryptography/OAuthWebApp/Controllers/LogInController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authentication;
+
+
+namespace OAuthWebApp.Controllers
+{
+    public class LogInController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return Challenge(new AuthenticationProperties()
+            {
+                RedirectUri = "/signin/index"
+            }, "Microsoft");
+
+        }
+
+    }
+}
