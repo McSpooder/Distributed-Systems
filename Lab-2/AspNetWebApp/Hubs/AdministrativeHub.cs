87f7f16f70d5c6ce9569b2c80374aa3f8c102778
danny-desktop <hextra@live.com>
2020-03-02 16:36:43 +0000

    Added an administrative hub to check if a username is valid.

---
+++ Lab-2/AspNetWebApp/Hubs/AdministrativeHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AspNetWebApp.Hubs
+{
+    public class AdministrativeHub : Hub
+    {
+        public async Task CheckUsr(string username)
+        {
+        }
+
+
+    }
+}
---
+++ Lab-2/ServerBackEnd/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR.Client;
+
+
+namespace ServerBackEnd
+{
+    class Program
+    {
+        static protected HubConnection connection;
+        static List<string> taken_usernames = new List<string>();
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            connection = new HubConnectionBuilder().WithUrl("http://localhost:51259/ChatHub").Build();
+        }
+
+        static bool checkAvailability(string pUsr)
+        {
+            foreach (string usr in taken_usernames)
+            {
+                if (pUsr == usr)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+    }
+}
